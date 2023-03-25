a6e83c35a35c2c8fef18be2fe48393d89d88945f
Dan <dan.laratta.jr@gmail.com>
2023-03-24 23:22:45 -0400

    some style and animation

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const Container = styled.div`
     
--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
-const Squares = styled.button`
+const Squares = styled(motion.button)`
     width: 15rem;
     height: 15rem;
     background-color: ${props => props.squareColor};
@@ -13,45 +14,70 @@ const Squares = styled.button`
     margin: 1rem;
 
     &:hover {
-        cursor: pointer;
         background-color:  ${props => props.hoverColor};
+        border: 0.4rem solid #fff;
+        cursor: ${props => props.disabled === 1 ? "default" : "pointer"};
     }
 `
 
-const Text = styled.p`
+const Text = styled(motion.p)`
     color: #fff;
     font-size: 10rem;
     font-weight: 700;
 `
 
+// variants
+const HoverVariants = {
+    nothing: {
+      scale: 1
+    },
+
+    hover: {
+        scale: 0.8,
+        transition: {
+            duration: 0.7,
+            repeat: Infinity,
+            repeatType: 'mirror',
+        }
+    }
+  };
+
 const Square = ({ value, sqaureClick }) => {
     const [squareColor, setSquareColor] = useState('')
     const [hoverColor, setHoverColor] = useState('')
-    const [border, setBorder] = useState(0)
+    const [disabled, setDisabled] = useState(0)
 
     useEffect(() => {
         if(value === 'X') {
             setSquareColor('#ff680a')
-            setHoverColor('#cc550c')
-            setBorder(1)
+            setHoverColor('rbga(0, 0, 0, 0)')
+            setDisabled(1)
         }
         else if(value === 'O') {
             setSquareColor('#08aace')
-            setHoverColor('#087a94')
-            setBorder(1)
+            setHoverColor('rbga(0, 0, 0, 0)')
+            setDisabled(1)
         }
         else {
             setSquareColor('#414141')
             setHoverColor('#737171')
-            setBorder(0)
+            setDisabled(0)
         }
 
-    }, [value, squareColor, hoverColor, border])
+    }, [value, squareColor, hoverColor])
 
   return (
     <>
-        <Squares onClick={ sqaureClick } squareColor={squareColor} hoverColor={hoverColor}> 
-            <Text> { value } </Text>
+        <Squares 
+            onClick={ sqaureClick } 
+            squareColor={squareColor} 
+            hoverColor={hoverColor}
+            disabled={disabled}
+            initial="nothing" 
+            whileHover="hover" 
+            animate="nothing" 
+        > 
+            <Text variants={HoverVariants}> { value } </Text>
         </Squares>
     </>
   )
